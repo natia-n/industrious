aa36e2b95f936067160a5b4cad8ea9de31fe3b6c
Natia <n.nakashidze09@gmail.com>
2020-11-03 01:25:23 +0400

    menu-s sheqmna  gamodzaxebis damateba js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const menuSia=document.getElementById('menuSia'); //gamochndes//
+const divMenu=document.getElementById('divMenu'); //movusminot//
+
+divMenu.addEventListener('click', menu);
+
+function menu (e){
+    e.preventDefault(); //a ლინკზე რომ არ გადავიდეს ან განახლება არ გააკეთოს//
+    menuSia.classList.toggle("show"); //თუ აქვს show- გახადოს none, თუ არა პირიქით block, სულ მოახდინოს ჩანაცვლება//
+};
